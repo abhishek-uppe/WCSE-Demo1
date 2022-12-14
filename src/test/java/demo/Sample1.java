a6bd09ed9b1030438e7065a4e3ceb7cf76770df6
abhishek-uppe <pallaviuppe006@gmail.com>
2022-12-14 18:11:36 +0530

    Sample test is updated

---
+++ src/test/java/demo/Sample1.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class Sample1 {
+	
+		@Test
+		public void SampleTest1()
+		{
+			System.out.println("hi this is sample");
+			
+		}
+
+}
